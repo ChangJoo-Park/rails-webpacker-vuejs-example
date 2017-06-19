e843511b3b012a18bc22fdf13e38656ca2f8c70a
ChangJoo Park <pcjpcj2@gmail.com>
2017-06-19 22:28:33 +0900

    Add app page links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  get 'home/app'
+  get '/app', to: 'home#app'
 
   root 'home#index'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
