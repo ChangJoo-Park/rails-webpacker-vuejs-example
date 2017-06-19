9e3dc2a2167851bb5262e47eaf18e019ebab3275
ChangJoo Park <pcjpcj2@gmail.com>
2017-06-19 22:40:20 +0900

    Add foreman and webpack dev server

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,9 +7,7 @@
 import Vue from 'vue'
 import App from './app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(document.createElement('hello'))
-  const app = new Vue(App).$mount('hello')
+document.body.appendChild(document.createElement('hello'))
+const app = new Vue(App).$mount('hello')
 
-  console.log(app)
-})
+console.log(app)
